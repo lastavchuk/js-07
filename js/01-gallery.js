195e672f2b7c81f302129c0d4ecf4f780e100c0d
lastavchuk <it.lastavchuk@gmail.com>
2023-06-06 18:23:03 +0300

    add

---
+++ js/01-gallery.js
@@ -0,0 +1,41 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+function render(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+           <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>`
+    )
+    .join("");
+}
+
+const gallery = document.querySelector(".gallery");
+
+gallery.innerHTML = render(galleryItems);
+
+let instanceImg = {};
+
+gallery.addEventListener("click", event => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") return;
+
+  instanceImg = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" alt="${event.target.alt}" width="1280">`
+  );
+  instanceImg.show();
+});
+
+document.body.addEventListener("keyup", event =>
+  event.code === "Escape" ? instanceImg.close() : ""
+);
---
+++ js/02-lightbox.js
@@ -0,0 +1,23 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+function render(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+        </li>`
+    )
+    .join("");
+}
+
+const gallery = document.querySelector(".gallery");
+
+gallery.innerHTML = render(galleryItems);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
